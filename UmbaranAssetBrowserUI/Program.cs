e1a534650544a3c798b4a86d8c6d70b2cec1352b
chenk017 <sukmaumbaran01@gmail.com>
2026-08-27 06:45:22 -0400

    Update Program.cs

--- UmbaranAssetBrowserUI/Program.cs
+++ UmbaranAssetBrowserUI/Program.cs
@@ -5,17 +5,16 @@ namespace UmbaranAssetBrowserUI;
 
 class Program
 {
-    [STAThread]
-    public static void Main(string[] args)
-    {
-        BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
-    }
-
     public static AppBuilder BuildAvaloniaApp()
     {
         return AppBuilder.Configure<App>()
-            .UsePlatformDetect()
+            .UseAndroid()
             .LogToTrace();
+    }
+
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp()
+            .StartWithAndroidLifetime(args);
     }
 }
